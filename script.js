c26d16e854426e115bf24f7b1504aa507833770f
pv56dd4czr-hub <pv56dd4czr@privaterelay.appleid.com>
2026-08-24 08:55:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-alert("SOMA SIGNATURE JavaScript is working!");
+alert("SOMASIGNATURES JavaScript is working!");
 const buttons = document.querySelectorAll(".product button");
 
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
     const product = button.parentElement.querySelector("h3").textContent;
 
-    const message = `Hello SOMA SIGNATURE ❤️ I am interested in ${product}. Please send me more details.`;
+    const message = `Hello SOMASIGNATURES ❤️ I am interested in ${product}. Please send me more details.`;
 
     const whatsappLink =
       "https://wa.me/2348037708522?text=" + encodeURIComponent(message);
